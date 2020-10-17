878747331e9cb73949824c1bb6c68b2d3678799f
HU Yueteng <yuetenghu@gmail.com>
2020-10-17 17:00:16 -0400

    Moved api to real data api (JPA)

--- src/api/ApiConfig.js
+++ src/api/ApiConfig.js
@@ -1,4 +1,5 @@
 import { LOCATIONIQ_ACCESS_TOKEN } from "../config/secret";
 
 export const C2H_API_URL = "http://localhost:8080";
+export const C2H_API_URL_JPA = "http://localhost:8080/jpa";
 export const GEOCODING_API_URL = `https://us1.locationiq.com/v1/search.php?key=${LOCATIONIQ_ACCESS_TOKEN}&format=json`;
