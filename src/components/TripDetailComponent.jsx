4216c6b4bcea618c4f215f718d2782d652ce43c0
HU Yueteng <yuetenghu@gmail.com>
2020-10-18 03:13:23 -0400

    Added SSL HTTPS and show current location

--- src/components/TripDetailComponent.jsx
+++ src/components/TripDetailComponent.jsx
@@ -5,9 +5,13 @@ import MapMarkerComponent from "./map_marker/MapMarkerComponent";
 import TimeUtils from "../utils/TimeUtils";
 
 class TripDetailComponent extends Component {
-    // constructor(props) {
-    //     super(props);
-    // }
+    constructor(props) {
+        super(props);
+        this.state = {
+            showCurrentPosition: false,
+            currentPosition: null
+        }
+    }
 
     static defaultProps = {
         center: {
@@ -18,12 +22,24 @@ class TripDetailComponent extends Component {
     };
 
     render() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    this.setState({
+                        currentPosition: position,
+                        showCurrentPosition: true
+                    })
+                    // currentPosition.lat = position.coords.latitude;
+                    // currentPosition.lng = position.coords.longitude;
+                }
+            )
+        }
         return (
             <>
                 <h4>Start time: {TimeUtils.toDisplayString(this.props.tripDetails.startTime)}</h4>
                 <h4>Passcode: <code>{this.props.tripDetails.passcode}</code></h4>
                 <h5>Total: {this.props.tripDetails.route.length}, Arrived: {this.props.tripDetails.route.filter(addr => addr.arrivalTime !== null).length}</h5>
-                {!this.props.tripDetails.isRouteUpToDate && <div class="alert alert-warning" role="alert">Still calculating route. Please use the bottom "Refresh" button to refresh.</div>}
+                {!this.props.tripDetails.isRouteUpToDate && <div className="alert alert-warning" role="alert">Still calculating route. Please use the bottom "Refresh" button to refresh.</div>}
                 {/* Important! Always set the container height explicitly */}
                 <div style={{ height: '50vh', width: '100%' }}>
                     <GoogleMapReact
@@ -34,6 +50,7 @@ class TripDetailComponent extends Component {
                         {this.props.tripDetails.route.map(
                             (addr) => <MapMarkerComponent key={addr.id} lat={addr.lat} lng={addr.lng} text={this.props.tripDetails.route.indexOf(addr) + 1} />
                         )}
+                        {this.state.showCurrentPosition && <MapMarkerComponent lat={this.state.currentPosition.coords.latitude} lng={this.state.currentPosition.coords.longitude} text="X" />}
                     </GoogleMapReact>
                 </div>
             </>
