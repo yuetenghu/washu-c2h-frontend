3e988cda0c9876306dd716ccc781e3250e592aa7
Yueteng <yuetenghu@gmail.com>
2020-11-10 23:42:05 -0500

    Minor fix

---
+++ src/config/config.js
@@ -0,0 +1,16 @@
+export const STATUS = Object.freeze({
+    GRAD: "Grad student",
+    UNDERGRAD: "Undergrad student",
+    STAFF: "Staff",
+})
+
+export const STATIONS = Object.freeze({
+    MALLINCKRODT: "Mallinckrodt (near Campus Store)",
+    HOYT_FORSYTH: "Hoyt/Forsyth"
+});
+
+export const ROLE = Object.freeze({
+    DRIVER: "Driver",
+    RIDER: "Rider",
+    ADMIN: "Admin"
+})
